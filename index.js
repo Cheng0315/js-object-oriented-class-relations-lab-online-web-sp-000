43377ac1947bb518cf8f56f9cd82afa189deaf5c
Cheng Vue <csalvue@gmail.com>
2019-02-26 17:17:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let store = {drivers: [], passenger: []}
+let store = {drivers: [], passengers: []}
 let driverId = 0;
 
 class Driver {
@@ -6,5 +6,13 @@ class Driver {
     this.id = ++driverId;
     this.name = name;
     store.drivers.push(this)
+  }
+}
+
+class Passenger {
+  constructor(name) {
+    this.id = ++driverId;
+    this.name = name;
+    store.passengers.push(this)
   }
 }
