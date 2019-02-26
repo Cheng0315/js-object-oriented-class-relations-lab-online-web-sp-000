9f934b6579eb8485e12b45d98dec95f04dec579a
Cheng Vue <csalvue@gmail.com>
2019-02-26 17:17:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ let driverId = 0;
 
 class Driver {
   constructor(name) {
-    this.id = ++driverId;
     this.name = name;
     store.drivers.push(this)
   }
@@ -11,7 +10,6 @@ class Driver {
 
 class Passenger {
   constructor(name) {
-    this.id = ++driverId;
     this.name = name;
     store.passengers.push(this)
   }
